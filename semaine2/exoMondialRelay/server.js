c38b1808c1872512baddd224b5813ce02532a69f
luisd57 <luisrma7@hotmail.com>
2023-06-07 15:27:48 +0200

    modified response to contain only JSON object

--- semaine2/exoMondialRelay/server.js
+++ semaine2/exoMondialRelay/server.js
@@ -37,7 +37,7 @@ app.post('/relaysearch', async (req, res) => {
     try {
         const client = await soap.createClientAsync(url);
         const result = await client.WSI3_PointRelais_RechercheAsync(args);
-        res.status(200).json(result);
+        res.status(200).json(result[0]);
     } catch (err) {
         res.status(500).send(err.toString());
     }
