9cb91640cf3291dc39751a66b864420f5f7786ba
luisd57 <luisrma7@hotmail.com>
2023-07-06 20:01:52 +0200

    furniture list filtering by category

--- semaine3/sujet2/app/src/app/components/furniture-list/furniture-list.component.ts
+++ semaine3/sujet2/app/src/app/components/furniture-list/furniture-list.component.ts
@@ -10,6 +10,8 @@ import { FurnitureService } from 'src/app/services/furniture.service';
 export class FurnitureListComponent implements OnInit {
 
   furnitureList: Furniture[] = [];
+  filteredList: Furniture[] = [];
+  currentCategory: string = '';
 
   constructor(private furnitureService: FurnitureService) { }
 
@@ -17,6 +19,7 @@ export class FurnitureListComponent implements OnInit {
     this.furnitureService.listFurniture().subscribe({
       next: (furnitureList: Furniture[]) => {
         this.furnitureList = furnitureList;
+        this.filteredList = furnitureList;
       },
       error: (error) => {
         console.error('Error fetching furniture list');
@@ -24,6 +27,13 @@ export class FurnitureListComponent implements OnInit {
       }
     });
   }
-
+  filterByCategory(category: string = ''): void {
+    this.currentCategory = category;
+    if (category) {
+        this.filteredList = this.furnitureList.filter(item => item.category === category);
+    } else {
+        this.filteredList = this.furnitureList;
+    }
+}
 
 }
